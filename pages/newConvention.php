240b61018a60fb0393b9b1ec973ebff49b07e909
AymericT <47082861+M-REEK@users.noreply.github.com>
2019-05-30 15:06:52 +0200

    test

--- pages/newConvention.php
+++ pages/newConvention.php
@@ -9,27 +9,28 @@
 			</div>
 			<div class="form">
 				<label for="">Entreprise</label>
-				<input type="text" name="" value="" placeholder="Nom entreprise">
-				<input type="text" name="" value="" placeholder="ID entreprise">
+				<input type="text" name="nomEntreprise" value="" placeholder="Nom entreprise">
+				<input type="text" name="idEntreprise" value="" placeholder="ID entreprise">
 			</div>
 			<div class="form">
 				<label for="">Etudiant</label>
-				<input type="text" name="" value="" placeholder="Nom Prénom Etudiant">
-				<input type="text" name="" value="" placeholder="num étudiant">
+				<input type="text" name="nomEtudiant" value="" placeholder="Nom Etudiant">
+				<input type="text" name="prenomEtudiant" value=""placeholder="Prénom Etudiant">
+				<input type="text" name="numEtudiant" value="" placeholder="num étudiant">
 			</div>
 			<div class="form">
-				<label for="">Date de début</label>
-				<input type="text" name="" value="">
-				<label for="">Date de fin</label>
-				<input type="text" name="" value="">
+				<label for="dateDebut">Date de début</label>
+				<input id="dateDebut" type="text" name="dateDebut" value="">
+				<label for="dateFin">Date de fin</label>
+				<input id="dateFin" type="text" name="dateFin" value="">
 			</div>
 			<div class="form">
 				<label for="">Montant</label>
-				<input type="text" name="" value="">
+				<input type="text" name="montant" value="">
 			</div>
 			<div class="form">
-				<label for="">Sujet</label>
-				<input type="text" name="" value="">
+				<label for="sujet">Sujet</label>
+				<input id="sujet" type="text" name="sujet" value="">
 			</div>
 			<button>Ajouter la convention</button>
 		</form>
