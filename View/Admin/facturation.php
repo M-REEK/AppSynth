9f606bb85232492c8ded6292d898b1d123cd0ebd
AymericT <47082861+M-REEK@users.noreply.github.com>
2019-06-07 10:31:23 +0200

    maj facturation.php + css done

--- View/Admin/facturation.php
+++ View/Admin/facturation.php
@@ -4,15 +4,13 @@
         <?php foreach ($allFacture as $fac): ?>
             <div class="facture">
                 <div>
-                    <p>Convention n° : <?= $fac['id_convention'] ?></p>
+                    <p>Convention n° <?= $fac['id_convention'] ?></p>
                     <p>Sujet : <?= $fac['sujet'] ?></p>
                     <p>Montant : <?= $fac['montant'] ?></p>
                     <p>Montant_reglé : <?= $fac['montant_regle'] ?></p>
                     <p>Montant_du : <?= $fac['montant']-$fac['montant_regle'] ?></p>
                     <p><a href="nouveauReglement?id=<?= $fac['id_convention'] ?>">Ajouter un reglement à la convention<i class="fas fa-plus-circle"></i></a></p>
                 </div>
-
-
             </div>
         <?php endforeach; ?>
     </section>
