df1971483ab14c506bc65575bcdb5d884f7f3e0d
AymericT <47082861+M-REEK@users.noreply.github.com>
2019-06-07 09:47:18 +0200

    maj ficheEtudiant.php done

--- View/Admin/ficheEtudiant.php
+++ View/Admin/ficheEtudiant.php
@@ -4,8 +4,7 @@
     <h1>Fiche etudiant de  <span><?= $etudiant['prenom'] ?> <?= $etudiant['nom'] ?></span> </h1>
       <p>n°etudant: <?= $etudiant['login'] ?></p>
       <p>Date de naissance <?= $etudiant['dateDeNaissance'] ?></p>
-      <p>Adresse: <?= $etudiant['adresse'] ?></p>
-      <p>Code postal: <?= $etudiant['code_postal'] ?></p>
+      <p>Adresse: <?= $etudiant['adresse'] ?>, <?= $etudiant['code_postal'] ?></p>
       <p>Portable: <?= $etudiant['telephone_portable'] ?></p>
       <p>Mail: <?= $etudiant['email'] ?></p>
     </section>
